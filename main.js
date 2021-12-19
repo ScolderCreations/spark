4a45aa1ed106ec7286361b17f403db64580e104c
ScolderCreations <ozor1xx@gmail.com>
2021-12-19 09:25:59 -0800

    added fetch

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true
+      webviewTag: true,
+      nodeIntegration: true
     }
   })
 
--- renderer.js
+++ renderer.js
@@ -1,8 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 const view1 = document.getElementById("view1")
+const box = document.getElementById("search")
+
+box.addEventListener("keypress", (e) => {
+    var result = fetch(box).then(res => {
+        return result
+    })
+    
+})
 view1.src = "file://" + __dirname + "/src/current/index.html"
