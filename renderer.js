2fe8c5bdbb9f7a12442eb636d20e0771622d3677
ScolderCreations <ozor1xx@gmail.com>
2021-12-19 09:27:18 -0800

    added key detetc

--- renderer.js
+++ renderer.js
@@ -1,10 +1,11 @@
-const view1 = document.getElementById("view1")
-const box = document.getElementById("search")
+const view1 = document.getElementById("view1");
+const box = document.getElementById("search");
 
 box.addEventListener("keypress", (e) => {
-    var result = fetch(box).then(res => {
-        return result
-    })
-    
-})
-view1.src = "file://" + __dirname + "/src/current/index.html"
+  if (e.keyCode === 13) {
+    var result = fetch(box).then((res) => {
+      return result;
+    });
+  }
+});
+view1.src = "file://" + __dirname + "/src/current/index.html";
